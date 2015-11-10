b8a8262f4a8b2e7fd94fd5adc0414f4529812cd8
danielpfernandes <daniel.paivafernandes@gmail.com>
2015-11-09 19:49:43 -0500

    Trying to solve Android video playback problem
    Function plays.js isn't working properly. A function was created to be
    executed each 13s cycle on Update () function. However, when the user
    touch on the scene to leave to main menu, the app persists to keep on
    the energizing_level scene.

--- Assets/Scripts/play.js
+++ Assets/Scripts/play.js
@@ -1,9 +1,22 @@
 ﻿#pragma strict
 
-var movTexture : MovieTexture;
+//var movTexture : MovieTexture;
 
-function Start () {
-	movTexture.loop = true;
-	GetComponent.<Renderer>().material.mainTexture = movTexture;
-	movTexture.Play();
+function Update () {
+	//movTexture.loop = true;
+	//GetComponent.<Renderer>().material.mainTexture = movTexture;
+	CoroutinePlayMovie ();	
+	new WaitForSeconds(12.0f);
+	if(Input.GetMouseButtonDown(0) || Input.touchCount >= 1) {
+		Application.LoadLevel("menu_main");
+	}
+	//movTexture.Play();
+}
+function CoroutinePlayMovie () {
+	Handheld.PlayFullScreenMovie (
+		"fluidAndroid.mp4", 
+		Color.black, 
+		FullScreenMovieControlMode.CancelOnInput,
+		FullScreenMovieScalingMode.Fill
+		);
 }
