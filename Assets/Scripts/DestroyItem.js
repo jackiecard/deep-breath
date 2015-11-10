c442ea8b761a4af2286453ed20545f2f0f18550d
danielpfernandes <daniel.paivafernandes@gmail.com>
2015-11-10 13:35:02 -0500

    Interface adjustments and exercises tutorial
    Removed Setting button
    Adjusted Help button position
    Created text description for exercises

---
+++ Assets/Scripts/DestroyItem.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+	//if(Input.GetMouseButtonDown(0)){
+	//	Destroy(this.gameObject);
+	//}
+}
+
+function Update () {
+	if(Input.GetMouseButtonDown(0)){
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/FadeText.js
@@ -0,0 +1,22 @@
+﻿ #pragma strict
+ 
+ var color : Color;
+ 
+ function Start(){
+     
+     color = Color.white;
+     
+ }
+ 
+ function Update(){
+ 
+    // Fade();
+ }
+  
+ function Fade(){
+ 
+     while (GetComponent.<GUIText>().material.color.a > 0){
+         GetComponent.<GUIText>().material.color.a -= 0.1 * Time.deltaTime * 0.05;
+     yield;
+     }
+ }
