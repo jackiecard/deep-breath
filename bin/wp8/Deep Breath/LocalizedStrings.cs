aeb04ff1d72c35135eb96062e01078521426fef9
danielpfernandes <daniel.paivafernandes@gmail.com>
2015-11-11 16:57:34 -0500

    Multiplatform deployment files

---
+++ bin/wp8/Deep Breath/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+using Deep_Breath.Resources;
+
+namespace Deep_Breath
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
