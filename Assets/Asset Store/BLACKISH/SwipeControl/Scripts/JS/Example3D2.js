bce34ed5bc73c3f3227565a95a26850babf726be
danielpfernandes <daniel.paivafernandes@gmail.com>
2015-12-15 10:44:26 -0500

    loading screens and features optmization
    Added loading screens between scenes
    Compressed audio
    Change Help screen
    Added back button on exercises screens

--- Assets/Asset Store/BLACKISH/SwipeControl/Scripts/JS/Example3D2.js
+++
@@ -1,9 +0,0 @@
-
-var swipeCtrl : SwipeControlFor3D;
-
-function Update () {
-	
-	transform.eulerAngles.y = swipeCtrl.smoothValue * 90.0;
-	
-	
-}
---
+++ Assets/Scripts/CreateBackButton.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Start () {
+	WaitForSeconds(7.0f);
+	
+}
+
+function Update () {
+	if(Input.GetMouseButtonDown(0) && WaitForSeconds(7.0f)){
+	Application.LoadLevel("menu_main");
+	}
+}
